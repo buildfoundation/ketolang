9db94f160653a5e10a2c7576777d51afdfcbbb85
Artem Zinnatullin <ceo@artemzin.com>
2022-06-03 02:02:17 +0400

    Rewrite property validation to collect all found issues

--- kotlinc/compiler-plugin-logic/src/main/kotlin/com/pushtorefresh/ketolang/compilerplugin/logic/PropertyValidator.kt
+++ kotlinc/compiler-plugin-logic/src/main/kotlin/com/pushtorefresh/ketolang/compilerplugin/logic/PropertyValidator.kt
@@ -14,24 +14,28 @@ import org.jetbrains.kotlin.ir.util.isEnumClass
 fun validateProperty(
     moduleFragment: IrModuleFragment,
     property: IrPropertyImpl
-): KetolangValidationError? {
+): List<KetolangValidationError> {
     val parent = property.parent
     return when {
         parent is IrClass -> {
             when {
                 parent.isData -> validateDataClassProperty(moduleFragment, property)
                 parent.isEnumClass -> validateEnumProperty(moduleFragment, property)
-                else -> KetolangValidationError(
-                    "Ketolang error: property looks suspicious! Perhaps Ketolang needs an update to validate it",
-                    property
+                else -> listOf(
+                    KetolangValidationError(
+                        "Ketolang error: property looks suspicious! Perhaps Ketolang needs an update to validate it",
+                        property
+                    )
                 )
             }
         }
 
         property.isTopLevel -> validateTopLevelProperty(moduleFragment, property)
-        else -> KetolangValidationError(
-            "Ketolang error: property looks suspicious! Perhaps Ketolang needs an update to validate it",
-            property
+        else -> listOf(
+            KetolangValidationError(
+                "Ketolang error: property looks suspicious! Perhaps Ketolang needs an update to validate it",
+                property
+            )
         )
     }
 }
@@ -40,126 +44,134 @@ fun validateProperty(
 private fun validateTopLevelProperty(
     moduleFragment: IrModuleFragment,
     property: IrPropertyImpl,
-): KetolangValidationError? {
-    val type by lazy(LazyThreadSafetyMode.NONE) { property.backingField?.type }
-
+): List<KetolangValidationError> {
     if (property.isConst) {
-        return null
-    } else if (type?.isPrimitiveType() == true || type?.isString() == true) {
-        return KetolangValidationError(
+        return emptyList()
+    }
+
+    val errors = mutableListOf<KetolangValidationError>()
+
+    val type = property.backingField?.type
+
+    if (type?.isPrimitiveType() == true || type?.isString() == true) {
+        errors += KetolangValidationError(
             "Ketolang error: primitive and String properties must be declared as 'const'",
             property
         )
     }
 
     if (property.isLateinit) {
-        return KetolangValidationError("Ketolang error: lateinit properties are not allowed!", property)
+        errors += KetolangValidationError("Ketolang error: lateinit properties are not allowed!", property)
     }
 
     if (property.isVar) {
-        return KetolangValidationError(
+        errors += KetolangValidationError(
             "Ketolang error: mutable properties are not allowed!",
             property
         )
     }
 
-    if (type?.classOrNull?.descriptor?.isData == true) {
-        return null
-    }
-
-    // TODO fix isCollection to actually check if is assignable from
-    if (type?.isSomeCollection(moduleFragment) == true) {
-        if (type?.isImmutableCollection(moduleFragment) == true) {
-            return null
-        } else {
-            return KetolangValidationError(
+    errors += when {
+        (type?.classOrNull?.descriptor?.isData == true) -> emptyList()
+        (type?.isSomeCollection(moduleFragment) == true && !type?.isImmutableCollection(moduleFragment)) -> listOf(
+            KetolangValidationError(
                 "Ketolang error: mutable collection properties are not allowed!",
                 property
             )
-        }
-    }
+        )
 
-    if (type?.isArray() == true) {
-        return KetolangValidationError(
-            "Ketolang error: top-level array properties are not allowed because arrays are mutable",
-            property
+        (type?.isArray() == true) -> listOf(
+            KetolangValidationError(
+                "Ketolang error: top-level array properties are not allowed because arrays are mutable",
+                property
+            )
+        )
+
+        (property.isDelegated) -> listOf(
+            KetolangValidationError(
+                "Ketolang error: delegated properties are not allowed!",
+                property
+            )
+        )
+
+        else -> listOf(
+            KetolangValidationError(
+                "Ketolang error: property looks suspicious! Perhaps Ketolang needs an update to validate it",
+                property
+            )
         )
     }
 
-    if (property.isDelegated) {
-        return KetolangValidationError("Ketolang error: delegated properties are not allowed!", property)
-    }
 
-    return KetolangValidationError(
-        "Ketolang error: property looks suspicious! Perhaps Ketolang needs an update to validate it",
-        property
-    )
+    return errors
 }
 
 @OptIn(ObsoleteDescriptorBasedAPI::class)
 private fun validateDataClassProperty(
     moduleFragment: IrModuleFragment,
     property: IrPropertyImpl,
-): KetolangValidationError? {
-    val type by lazy(LazyThreadSafetyMode.NONE) { property.backingField?.type }
+): List<KetolangValidationError> {
+    val errors = mutableListOf<KetolangValidationError>()
 
     if (property.isLateinit) {
-        return KetolangValidationError("Ketolang error: lateinit properties are not allowed!", property)
+        errors += KetolangValidationError("Ketolang error: lateinit properties are not allowed!", property)
     }
 
     if (property.isVar) {
-        return KetolangValidationError(
+        errors += KetolangValidationError(
             "Ketolang error: mutable properties are not allowed!",
             property
         )
     }
 
-    if (type?.isPrimitiveType() == true
-        || type?.isString() == true
-        || type?.classOrNull?.descriptor?.isData == true
-    ) {
-        return null
-    }
+    val type = property.backingField?.type
 
-    if (type?.isSomeCollection(moduleFragment) == true) {
-        if (type?.isImmutableCollection(moduleFragment) == true) {
-            return null
-        } else {
-            return KetolangValidationError(
+    errors += when {
+        (type?.isPrimitiveType() == true
+                || type?.isString() == true
+                || type?.classOrNull?.descriptor?.isData == true
+                ) -> emptyList()
+
+        (type?.isSomeCollection(moduleFragment) == true && !type.isImmutableCollection(moduleFragment)) -> listOf(
+            KetolangValidationError(
                 "Ketolang error: mutable collection properties are not allowed!",
                 property
             )
-        }
-    }
+        )
 
-    if (type?.isArray() == true) {
-        return KetolangValidationError(
-            "Ketolang error: array properties are not allowed because arrays are mutable",
-            property
+        type?.isArray() == true -> listOf(
+            KetolangValidationError(
+                "Ketolang error: array properties are not allowed because arrays are mutable",
+                property
+            )
+        )
+
+        else -> listOf(
+            KetolangValidationError(
+                "Ketolang error: property looks suspicious! Perhaps Ketolang needs an update to validate it",
+                property
+            )
         )
     }
 
     if (property.isDelegated) {
-        return KetolangValidationError("Ketolang error: delegated properties are not allowed!", property)
+        errors += KetolangValidationError("Ketolang error: delegated properties are not allowed!", property)
     }
 
-    return KetolangValidationError(
-        "Ketolang error: property looks suspicious! Perhaps Ketolang needs an update to validate it",
-        property
-    )
+    return errors
 }
 
 private fun validateEnumProperty(
     moduleFragment: IrModuleFragment,
     property: IrPropertyImpl
-): KetolangValidationError? {
+): List<KetolangValidationError> {
     if (property.backingField == null
         && (
                 property.name.asString() == "name"
                         || property.name.asString() == "ordinal"
                 )
     ) {
-        return null
+        return emptyList()
     } else {
         return validateDataClassProperty(moduleFragment, property)
     }
