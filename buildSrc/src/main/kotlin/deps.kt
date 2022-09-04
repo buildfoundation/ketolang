f1a6f570f5633578d8e74341adb90f5375bb625e
Artem Zinnatullin <ceo@artemzin.com>
2022-09-04 16:57:18 +0400

    Update Kotlin

--- buildSrc/src/main/kotlin/deps.kt
+++ buildSrc/src/main/kotlin/deps.kt
@@ -1,9 +1,9 @@
 // Common dependencies declaration for all project modules.
 
 object deps {
-    const val kotlin = "1.6.21"
+    const val kotlin = "1.7.10"
     const val kotlin_test = kotlin
-    const val kotlin_compile_testing = "1.4.8"
+    const val kotlin_compile_testing = "1.4.9"
     const val detekt = "1.21.0"
     const val junit = "5.9.0"
     const val gradle_versions_plugin = "0.42.0"
