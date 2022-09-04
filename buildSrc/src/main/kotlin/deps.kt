1aaffb2ca889704379c2175a4f934bca7c20830e
Artem Zinnatullin <ceo@artemzin.com>
2022-09-04 16:08:43 +0400

    Update Gradle, JUnit, Detekt

--- buildSrc/src/main/kotlin/deps.kt
+++ buildSrc/src/main/kotlin/deps.kt
@@ -4,8 +4,8 @@ object deps {
     const val kotlin = "1.6.21"
     const val kotlin_test = kotlin
     const val kotlin_compile_testing = "1.4.8"
-    const val detekt = "1.20.0"
-    const val junit = "5.8.2"
+    const val detekt = "1.21.0"
+    const val junit = "5.9.0"
     const val gradle_versions_plugin = "0.42.0"
     const val shadow_plugin = "7.1.2"
 }
